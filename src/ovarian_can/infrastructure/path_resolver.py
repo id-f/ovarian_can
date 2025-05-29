c4551b96e29b102d47884772d8ac7bef15d3c9bb
id-f <masaki108@gmail.com>
2025-05-29 16:20:39 +0900

    2nddataset

--- src/ovarian_can/infrastructure/path_resolver.py
+++ src/ovarian_can/infrastructure/path_resolver.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 class PathResolver:
     def get_raw_data_dir(self) -> Path:
-        return BASE_DATA_PATH / "raw" / "Xenium_Prime_Ovarian_Cancer_FFPE_XRrun_outs"
+        return BASE_DATA_PATH / "raw" / "Xenium_V1_Human_Ovarian_Cancer_Addon_FFPE_outs"
 
     def get_interim_data_dir(self) -> Path:
         return BASE_DATA_PATH / "interim"
